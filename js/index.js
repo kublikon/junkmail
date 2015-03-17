2e7138c9a24720efe88ee4d58690cd15dd5ff383
kuba <jszersze@gmail.com>
2015-03-17 16:47:44 -0700

    template update

--- js/index.js
+++ js/index.js
@@ -4,7 +4,8 @@
 	var domain = 'http://kublikon.github.io/junkmail/',
 		index = 1,
 		maxIndex = 5,
-		code = '';
+		code = '',
+		format = false;
 
 	changeColor(true);
 	getSample(index);
@@ -12,6 +13,12 @@
 	$('.controls span').click(function(data){
 		$('.controls span').removeClass('on');
 		$('#' + data.target.id).addClass('on');
+
+		if(data.target.id > index){
+			$('.b-move').addClass('bottom');
+		} else {
+			$('.b-move').addClass('top');
+		}
 
 		changeColor();
 
@@ -22,6 +29,8 @@
 	$('.up').click(function(){
 		if(index > 1){
 			index--;
+
+			$('.b-move').addClass('top');
 
 			$('.controls span').removeClass('on');
 			$('#' + index).addClass('on');
@@ -34,6 +43,8 @@
 	$('.down').click(function(){
 		if(index < maxIndex){
 			index++;
+
+			$('.b-move').addClass('bottom');
 
 			$('.controls span').removeClass('on');
 			$('#' + index).addClass('on');
@@ -50,51 +61,9 @@
 		$('.modal-back').show();
 
 		$.get(domain + 'templates/simple-' + index + '.html', function(data){
-			var tags = '',
-				indentCount = -1;
+			
 
-			data = data.split('<');
-
-			for (var t = 0; t < data.length; t++) {
-				if(data[t] != ''){
-					var indents = '';
-
-					data[t] = '<' + data[t];
-
-					// console.log(data[t], data[t].indexOf('</'));
-					// console.log(data[t], data[t].indexOf('>'));
-
-					if((t + 1) < data.length){
-
-						console.log(data[t+1].indexOf('</'));
-						console.log(data[t+1]);
-
-						if(data[t].indexOf('</') != -1){
-							indentCount--;						
-						} else if(data[t+1].indexOf('/') != -1){
-
-						} else if (data[t].indexOf('>') != -1) {
-							indentCount++;
-						}
-					}
-
-					for (var i = 0; i < indentCount; i++) {
-						indents += '&nbsp;';
-					};
-
-					data[t] = data[t].replace('<', '&lt;');
-					data[t] = data[t].replace('>', '&gt;');
-					data[t] = indents + data[t];
-					
-					tags += data[t] + '<br>';
-				}
-			};
-
-			data.forEach(function(t){
-				
-			});
-
-			$('#code').html(tags);
+			$('#code').html(formatCode(data));
 		});
 		
 	});
@@ -108,6 +77,11 @@
 	function getSample(number){
 		$.get(domain + 'assets/sample-' + number + '.html', function(data){
 			$('#template').html(data);
+
+			setTimeout(function(){
+				$('.b-move').removeClass('top');
+				$('.b-move').removeClass('bottom');
+			},300);
 		});
 	};
 
@@ -121,6 +95,79 @@
 			$('.b-color').animate({backgroundColor: randomColor}, 'slow');
 			$('.f-color').animate({color: randomColor}, 'slow');
 		}
+	};
+
+	function formatCode(data){
+		var tags = '',
+			indentCount = -1;
+
+		data = data.split('<');
+		data.shift();
+
+		for (var t = 0; t < data.length; t++) {
+			data[t] = '<' + data[t];
+		};
+
+		for (var t = 0; t < data.length; t++) {
+			if(data[t] != ''){
+				var indents = '',
+					br = '';
+
+				if((t + 1) < data.length){
+					if(format){
+
+						console.log(data[t].indexOf('<'), '<', data[t]);
+						console.log(data[t].indexOf('</'), '</');
+						console.log(data[t+1].indexOf('</'), '+</');
+						if(t-1 >= 0){
+							console.log(data[t-1].indexOf('</'), '-</');
+						}
+						console.log('---------------');
+
+						if(data[t].indexOf('</') == -1 && data[t+1].indexOf('</') == -1){
+							br = '<br>';
+							indentCount++;
+							console.log('> br');
+						} else if (data[t].indexOf('</') == -1 && data[t+1].indexOf('</') != -1){
+							indentCount++;
+							console.log('>');
+						} else if(data[t].indexOf('</') != -1 && data[t-1].indexOf('</') != -1) {
+							console.log('!');
+						}
+						// } else if (data[t].indexOf('</') != -1 && data[t-1].indexOf('/') == -1){
+						// 	// br = '<br>';
+						// 	indentCount--;
+						// }
+
+
+
+						// if(data[t].indexOf('</') != -1){
+						// 	br = '<br>';
+						// 	indentCount--;						
+						// } else if(data[t+1].indexOf('/') != -1){
+						// 	indentCount++;
+						// } else if (data[t-1].indexOf('/') == -1){
+
+						// } else if (data[t].indexOf('>') != -1) {
+						// 	br = '<br>';
+						// 	indentCount++;
+						// }
+					}
+				}
+
+				for (var i = 0; i < indentCount; i++) {
+					indents += '&nbsp;&nbsp;';
+				};
+
+				data[t] = data[t].replace('<', '&lt;');
+				data[t] = data[t].replace('>', '&gt;');
+				data[t] = indents + data[t];
+				
+				tags += data[t] + br;
+			}
+		};
+
+		return tags;
 	};
 
 })();
